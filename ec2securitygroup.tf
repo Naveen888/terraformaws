5e08f36e567df627c0f50cccbc54cb964537a390
Gopal <Gopal1409@gmail.com>
2021-08-02 06:16:13 -0700

    instance filter

--- ec2securitygroup.tf
+++ ec2securitygroup.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "vpc-web"{
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0"]
+    cidr_blocks      = ["0.0.0.0/0"]
     
   }
   ingress {
@@ -42,7 +42,7 @@ resource "aws_security_group" "vpc-web"{
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0"]
+    cidr_blocks      = ["0.0.0.0/0"]
     
   }
 
